3d383ccd6761d680f3766cc51259843fa248adb7
NikhilaR2006 <lakshminikhilar@gmail.com>
2025-05-17 13:02:48 +0530

    Update ReportDetailCard.jsx

--- src/components/ReportDetailCard.jsx
+++ src/components/ReportDetailCard.jsx
@@ -76,7 +76,7 @@ const ReportDetailCard = ({ report, onClose, markAsSeen, markAsResolved }) => {
       const formData = new FormData();
       formData.append('image', blob, 'upload.jpg');
 
-      const predictResponse = await fetch('http://localhost:5002/predict', {
+      const predictResponse = await fetch('https://huggingface.co/spaces/NikhilaR2006/safe-street-predict.py/predict', {
         method: 'POST',
         body: formData,
       });
